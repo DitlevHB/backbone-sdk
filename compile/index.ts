d2886e45508b10b132ff6444ae615dbbf4072442
jensen@backbonedao.com <jensen@backbonedao.com>
2022-07-18 14:20:53 +0000

    wip

--- compile/index.ts
+++ compile/index.ts
@@ -3,14 +3,12 @@ const { exec } = require("child_process")
 
 async function task(opts: {
   dir: string
-  name?: string
-  description?: string
 }) {
-  const { dir, name, description } = { ...opts }
+  const { dir } = { ...opts }
 
   // install dependencies
   log(`Compiling and minifying...`)
-  const cmd = `cd ${dir} && npm run build:browser:full && npm run build:browser:app`
+  const cmd = `cd ${dir} && npm run build:browser:full`
   return new Promise((resolve, reject) => {
     const npm = exec(cmd)
     let errors = false
@@ -25,17 +23,12 @@ async function task(opts: {
     npm.on("close", (code) => {
       resolve(true)
       if(!errors) {
-        log('Standalone project compiled to dist/exe.min.js (the full package, includes Backbone Core)')
-        log('App only compiled to dist/app.min.js (use this if you load Backbone Core from CDN)')
+        log('App compiled to dist/app.min.js and UI compiled to dist/ui.min.js')
+        log(`Protip: Run 'bb deploy' to get code checksum for release signing.`)
       }
       process.exit(0)
     })
   })
 }
-
-process.on('SIGINT', function() {
-  console.log("Caught interrupt signal")
-  process.exit()
-});
 
 export default task
