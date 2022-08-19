d68263d4e2711bba8a5d31be4db4f12e7e091355
jensen@backbonedao.com <jensen@backbonedao.com>
2022-08-19 10:16:43 +0000

    excluded dist

--- dist/lib/core/models/operation.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const base_1 = require("./base");
-const { Any } = require('./objectmodel');
-exports.default = (0, base_1.Object)({
-    type: String,
-    data: Any,
-});
--- dist/lib/core/models/user.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const base_1 = require("./base");
-const User = (0, base_1.Object)({
-    username: String,
-    public_key: String,
-});
-exports.default = User;
