d81088e5f473efa4ba79b7f145d1eb30ebe0e6a7
jensen@backbonedao.com <jensen@backbonedao.com>
2022-08-26 07:25:10 +0000

    added setup workflow

---
+++ utils/setup.ts
@@ -0,0 +1,67 @@
+import { log } from "../helper"
+const { exec } = require("child_process")
+
+;(async () => {
+  const dir = process.cwd()
+  log(`Installing NPM dependencies... (this can take a few minutes)`)
+  await runNPM(dir, "npx pnpm install")
+  log(`Installing Core...`)
+  await initSubmodules(dir)
+  log(`Installing Core NPM dependencies... (this can take a few minutes)`)
+  await runNPM(`${dir}/lib/core`, "npx pnpm install")
+  log(`Building Core...`)
+  await runNPM(`${dir}/lib/core`, "npm run build:node")
+  log(`Building SDK...`)
+  await runNPM(dir, "npm run build")
+  log("All done!")
+})()
+
+async function runNPM(dir, npmcmd) {
+  const cmd = `cd ${dir} && ${npmcmd}`
+  return new Promise((resolve, reject) => {
+    const npm = exec(cmd)
+    let errors = false
+    npm.stderr.on("data", (data) => {
+      if (data.match(/npm ERR/)) {
+        log(`${data}`, false, "red")
+        errors = true
+      }
+    })
+    npm.stdout.on("data", (data) => {
+      log(`${data}`)
+    })
+
+    npm.on("close", (code) => {
+      if (!errors) resolve(true)
+      else {
+        log(`Please fix the errors and try again`, false, "red")
+        process.exit(0)
+      }
+    })
+  })
+}
+
+async function initSubmodules(dir) {
+  const cmd = `cd ${dir} && git submodule init && git submodule pull`
+  return new Promise((resolve, reject) => {
+    const git = exec(cmd)
+    let errors = false
+    git.stderr.on("data", (data) => {
+      if (data.match(/ERR/)) {
+        log(`${data}`, false, "red")
+        errors = true
+      }
+    })
+    git.stdout.on("data", (data) => {
+      // log(`${data}`)
+    })
+
+    git.on("close", (code) => {
+      if (!errors) resolve(true)
+      else {
+        log(`Please fix the errors and try again`, false, "red")
+        process.exit(0)
+      }
+    })
+  })
+}
