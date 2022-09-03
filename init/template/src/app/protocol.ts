c3f6c27dc78d21b0048c26292a6887036b0906c9
DitlevHB <naranix@hotmail.com>
2022-09-03 14:14:36 +0530

    simplification of bb init sample app

--- init/template/src/app/protocol.ts
+++ init/template/src/app/protocol.ts
@@ -1,16 +1,6 @@
 export default async function(op, Data) {
   switch (op.type) {
-    case 'set': {
-      await Data.put({ key: op.key, value: op.value })
-      break
-    }
-    case 'del': {
-      const p = await Data.get(op.key)
-      if (!p) break
-
-      await Data.del(op.key)
-      break
-    }
+    // insert operation cases here
     default:
       return Data.discard(op, "unknown operation")
   }
--- init/template/src/ui/hello.tsx
+++ init/template/src/ui/hello.tsx
@@ -1,69 +1,28 @@
 import React from "react"
-import { useState, useEffect } from "react"
-import { bb, monitorDatabase } from "./helpers"
 
 export default function Hello() {
-  const [msgs, setMsgs] = useState([])
-  const [msg, setMsg] = useState("")
-
-  // get messages ONCE when component mounts
-  useEffect(() => {
-    refreshMsgs()
-    monitorDatabase(setMsgs)
-  }, [])
-
-  async function refreshMsgs() {
-    const msgs = bb() ? await bb().all() : []
-    setMsgs(msgs)
-  }
-
-  async function addMessage(e?) {
-    if (e) e.PreventDefault()
-    if (msg) {
-      bb() ? await bb().set({ key: new Date().getTime().toString(), value: msg }) : null
-      setMsg("")
-      await refreshMsgs()
-    }
-  }
-
-  return (
+   return (
     <>
-      <div className="head">Anonymous chat</div>
-      <div className="sub">
-        {!location.href.match(/#0x/)
-          ? `Open ${location.href}#${window["_dev_address"]} on another browser (not a tab)`
-          : ""}{" "}
+      <div className="head">Hello Backbone!</div>
+      <div className="sub vertical-space">
+        <div className="sub-headline">See the power of self-sovereign dapps</div> 
+        <ul className="list">
+          <li>
+            1. Keep this tab open.
+          </li>
+          <li>
+            2. Terminate the command line instance running this application.
+          </li>
+          <li>
+            3. Open {location.href} on another browser (not a tab).
+          </li>
+          <li>
+            4. The app is now served from your first tab.
+          </li>
+        </ul>
       </div>
-      <div id="messages">
-        <table>
-          <tbody>
-            {msgs.map((v, i) => {
-              return (
-                <tr key={i}>
-                  <td className="pointer">{">"}</td>
-                  <td className="message">{v}</td>
-                </tr>
-              )
-            })}
-          </tbody>
-        </table>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault()
-          }}
-        >
-          <input
-            type="text"
-            placeholder="what's on your mind?"
-            name="msg"
-            id="msg"
-            value={msg}
-            onChange={(e) => setMsg(e.target.value)}
-          />
-          <button type="submit" onClick={() => addMessage()}>
-            Post
-          </button>
-        </form>
+      <div className="sub vertical-space">
+        Check out <a href="https://github.com/backbonedao/demos" target="_blank">demo apps</a> for inspiration 
       </div>
     </>
   )
